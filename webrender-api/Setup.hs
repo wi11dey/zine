a9d0082fad390c0b0801e0d0feed095beb0a10ae
Will Dey <wdey@palantir.com>
2026-05-05 17:30:13 +0100

    setup hook

--- webrender-api/Setup.hs
+++ webrender-api/Setup.hs
@@ -1,2 +1,17 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+import Distribution.PackageDescription
+import Distribution.Simple.LocalBuildInfo
+import System.Process
+import System.Exit
+import Control.Monad
+
+main = defaultMainWithHooks simpleUserHooks
+  { preBuild = \args buildFlags -> do
+      putStrLn "Running cbindgen..."
+      exitCode <- system "cbindgen webrender_bindings_cbindgen -c webrender_bindings_cbindgen.toml -o webrender_bindings_cbindgen.h"
+      case exitCode of
+        ExitSuccess -> putStrLn "cbindgen completed successfully"
+        ExitFailure code -> error $ "cbindgen failed with exit code: " ++ show code
+      preBuild simpleUserHooks args buildFlags
+  }
