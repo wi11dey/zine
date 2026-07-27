c59c680dda1d1e8c2c3d2f180a0e90936377370b
Will Dey <wdey@palantir.com>
2026-07-27 15:40:21 +0100

    more

--- build.rs
+++ build.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use std::process::Command;
+use std::error::Error;
+
+fn qlot() -> Result<(), Box<dyn Error>> {
+    println!("cargo::rerun-if-changed=qlfile");
+    println!("cargo::rerun-if-changed=qlfile.lock");
+
+    Command::new("qlot").arg("install").status()?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    qlot()?;
+
+    Ok(())
+}
