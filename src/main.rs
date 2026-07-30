ae8ec7ed0e3af7e759b1f6ab22a13104e2e964e0
Will Dey <wdey@palantir.com>
2026-07-30 15:06:53 +0100

    more

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-mod ecl;
-
-fn main() {
-    let mut argv = [c"zine".as_ptr().cast_mut(), std::ptr::null_mut()];
-    unsafe {
-        ecl::cl_boot(1, argv.as_mut_ptr());
-        ecl::cl_shutdown();
-    }
-}
